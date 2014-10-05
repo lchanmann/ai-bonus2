f0180354c3ddfcaf1db0a5f56ac08c9068723197
Mann <chanmannlim@gmail.com>
2014-10-05 10:21:27 -0500

    add euclidean and combined heuristic to application

--- src/main/java/ai/apps/Application.java
+++ src/main/java/ai/apps/Application.java
@@ -1,8 +1,10 @@
 package ai.apps;
 
 import ai.core.Puzzle;
+import ai.core.heuristic.CombinedHeuristic;
 import ai.core.heuristic.Heuristic;
 import ai.core.heuristic.ManhattanDistance;
+import ai.core.heuristic.EuclideanDistance;
 import ai.core.search.AStarSearch;
 import ai.core.search.HillClimbingSearch;
 import ai.core.search.SearchAlgorithm;
@@ -20,15 +22,19 @@ public class Application {
      * The puzzle list
      */
     private Puzzle[] puzzles = new Puzzle[totalPuzzles];
-    // Puzzle puzzle = new Puzzle(new char[] {'8','6','4','5','7','*','3','2','1'});
+//    private Puzzle puzzle = new Puzzle(new char[] {'7','*','8','5','4','1','6','3','2'});
     
     private SearchResult[] aStarResults = new SearchResult[totalPuzzles];
     private SearchResult[] hillClimbingResults = new SearchResult[totalPuzzles];
 
     private Heuristic manhattan;
+    private Heuristic euclidean;
+    private Heuristic combined;
 
     public Application() {
         manhattan = new ManhattanDistance(Puzzle.GOAL_STATE);
+        euclidean = new EuclideanDistance(Puzzle.GOAL_STATE);
+        combined = new CombinedHeuristic(Puzzle.GOAL_STATE);
     }
 
     public void start() {
@@ -58,7 +64,7 @@ public class Application {
      * Hill-climbing search
      */
     private void performHillClimbingSearch() {
-        SearchAlgorithm search = new HillClimbingSearch(manhattan);
+        SearchAlgorithm search = new HillClimbingSearch(euclidean);
 
         for (int i=0; i<puzzles.length; i++) {
             hillClimbingResults[i] = search.solve(puzzles[i]);
